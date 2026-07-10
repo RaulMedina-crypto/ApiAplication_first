f7ceaeeb79533b7a773ac26d3e9b7c0fbb86aeea
RaulMedina-crypto <medinaortega.raul1@gmail.com>
2026-07-09 18:17:46 -0600

    Se agregan dependencias, modelo producto, metodoss productoService y applicationProperties

---
+++ src/main/java/org/generation/api/repositories/ProductoRepository.java
@@ -0,0 +1,9 @@
+package org.generation.api.repositories;
+
+import org.generation.api.modelos.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
